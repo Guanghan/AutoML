90d85d5a5adc31fb9e382972c608f3c1f36e20be
guanghan.ning <guanghan.ning@jd.com>
2020-10-16 11:39:03 -0700

    register the metrics evaluator

--- src/trainer/__init__.py
+++ src/trainer/__init__.py
@@ -15,3 +15,4 @@ from src.trainer.classifier_metrics import *
 from src.trainer.lr_scheduler_callback import *
 from src.trainer.pf_saver_callback import *
 from src.trainer.progress_logger import *
+from src.trainer.metrics_evaluator import *
